c3def95e854f98c915a84bb2985636a49a94b7cb
amangana32 <amangana@andrew.cmu.edu>
2026-02-07 14:34:49 -0500

    incorportated turning on LED matrix using array

--- virtualLED.py
+++ virtualLED.py
@@ -8,6 +8,7 @@ import tkinter as tk
 import time
 import plotly.express as px
 
+
 LEDs = np.zeros(shape=72)
 width = 12
 height = 6
@@ -38,30 +39,42 @@ plt.ylim(0,8)
 plt.title("LED Array")
 plt.legend()
 
-def graphUpdate(width,row,col,seconds):
+def graphUpdate(width, height, array,seconds):
+    index = 0
+    while index<width*height:
+        row = get_row(index,width)
+        col = get_col(index,width)
+        if array[index]==1:
+            print(f'turning on {row},{col}')
+            plt.scatter(row+1,col+1,color='Yellow')
+        else:
+            print(f'turning off {row},{col}')
+            plt.scatter(row+1,col+1,color='Gray')
+        index=index+1
+    
+    plt.pause(seconds)
+    plt.draw()
+    # update graph
+    #plt.draw()
+    plt.pause(.1)
 
-
-    while True:
-
-        # to fix
-        for led in LEDs:
-            turn_on(width,row,col)
-            plt.pause(seconds)
-            turn_off(width,row,col)
-            plt.pause(seconds)
-
-        # update graph
-        plt.draw()
-        plt.pause(.1)
-
-        # display graph
-        plt.tight_layout()
-        plt.show(block=False)
+    # display graph
+    plt.tight_layout()
+    plt.show(block=False)
 
 if __name__ == '__main__':
     print("Initializing test")
 
-    graphUpdate()
+    LEDs[14]=0
+    LEDs[18]=0
+    LEDs[2]=1
+    LEDs[9]=1
+    graphUpdate(width,height,LEDs,1)
+    LEDs[2]=0
+    LEDs[9]=0
+    LEDs[14]=1
+    LEDs[18]=1
+    graphUpdate(width,height,LEDs,1)
 
 
 
